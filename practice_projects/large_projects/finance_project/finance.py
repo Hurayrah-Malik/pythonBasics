10af9e6d1f26f929b4e14d1119aa43ab18c717a4
Hurayrah <hurayrah92@gmail.com>
2026-01-26 22:22:50 -0500

    almost finishing finance project

--- practice_projects/large_projects/finance_project/finance.py
+++ practice_projects/large_projects/finance_project/finance.py
@@ -82,16 +82,165 @@ This tool is designed as a local, dependency-light analysis utility and does
 not rely on external databases or third-party finance services.
 """
 
+import csv
+
 
 def main() -> None:
-    print("running main functino")
+    # get all the rows in a list
+    rows_list = read_csv()
+    print_list(rows_list)
+
+    # turn all rows into dictionaries
+    rows_dictionary = rows_to_dictionaries(rows_list)
+
+    # remove all the invalid rows
+    clean_and_dirty_rows = clean_rows(rows_dictionary)
+
+    clean = clean_and_dirty_rows["clean"]
+    dirty = clean_and_dirty_rows["dirty"]
+
+    # print the clean rows
+    print("\n clean rows")
+    print_list(clean)
+
+    # print the dirty ones
+    print("\n dirty rows")
+    print_list(dirty)
+
+    # get the income and spending amounts (in dict format)
+    income_and_spending = compute_income_and_spending(clean)
+
+    # extract them
+    income_cents = income_and_spending["total_income"]
+    spending_cents = income_and_spending["total_spending"]
+
+    print(f"total income in cents: {income_cents}")
+    print(f"total spending in cents: {spending_cents}")
 
 
-# turn a string from file to a dictionary
-def string_to_dictionary(line: str) -> dict | None:
-    pass
+# read the csv and output a list of all rows
+def read_csv() -> list:
+    rows = []
+    with open("transactions.csv", newline="") as f:
+        reader = csv.reader(f)
+        next(reader)  # skip header
+        for row in reader:
+            rows.append(row)
+    return rows
 
 
-# convert dollars into cents
-def convert_dollars(dollars: str) -> int:
-    return 0
+# get all the rows, and take out the invalid ones
+def clean_rows(rows: list) -> dict:
+
+    clean_and_dirty = {"clean": [], "dirty": []}
+    for i in range(len(rows)):
+
+        # first try to convert the dollars into pennies. if not able to, dont add to clean list.
+        try:
+            rows[i]["amount"] = dollar_to_penny(rows[i]["amount"])
+        except ValueError:
+            clean_and_dirty["dirty"].append(rows[i])
+            continue
+
+        # if every key in the dictionary has a value, then dont do anything
+        if (
+            rows[i]["date"]
+            and rows[i]["description"]
+            and rows[i]["amount"]
+            and rows[i]["category"]
+        ):
+            # make sure amount has correct symbol, then append to clean list
+            check_amount_symbol(rows[i])
+            clean_and_dirty["clean"].append(rows[i])
+        else:
+            clean_and_dirty["dirty"].append(rows[i])
+
+    return clean_and_dirty
+
+
+# takes a dictionary, makes sure the symbol of "amount" is correct
+def check_amount_symbol(row: dict) -> None:
+    category = row["category"]
+    amount = row["amount"]
+
+    if category == "Income" and amount < 0:
+        row["amount"] = -amount
+    elif category != "Income" and amount > 0:
+        row["amount"] = -amount
+
+    return
+
+
+# calculate the total income and the spending amount
+def compute_income_and_spending(clean_rows: list) -> dict:
+
+    income_and_spending = {"total_income": 0, "total_spending": 0}
+
+    for row in clean_rows:
+        category = row["category"]
+        amount = row["amount"]
+
+        if category == "Income":
+            income_and_spending["total_income"] += amount
+        else:
+            income_and_spending["total_spending"] += amount
+
+    # convert from negative to positive cents
+    income_and_spending["total_spending"] = -income_and_spending["total_spending"]
+
+    return income_and_spending
+
+
+# convert dollars to pennies
+def dollar_to_penny(dollars: str) -> int:
+    pennies = round(float(dollars) * 100)
+    return pennies
+
+
+# convert pennies to dollars
+def penny_to_dollar(pennies: int) -> float:
+    dollars = pennies / 100
+    return dollars
+
+
+# turn a single row into a dictionary
+def row_to_dictionary(row: list) -> dict:
+    keys = ["date", "description", "amount", "category"]
+    values = row
+
+    row_dictionary = {}
+
+    for i in range(len(keys)):
+        row_dictionary[keys[i]] = values[i]
+
+    return row_dictionary
+
+
+# turn every single row into a dictionary
+def rows_to_dictionaries(rows: list) -> list:
+    counter = 0
+    for row in rows:
+        row_dict = row_to_dictionary(row)
+        rows[counter] = row_dict
+        counter += 1
+
+    return rows
+
+
+# print list neatly
+def print_list(items):
+
+    i = 1
+    for item in items:
+        print(i, ".", item)
+        i += 1
+
+
+# print dictionary neatly
+def print_dict(d):
+    for key in d:
+        print(key, ":", d[key])
+
+
+if __name__ == "__main__":
+    main()
